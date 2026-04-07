3bb60abefedc2915245b6964182cc59c5bd0674d
Salomon BRYS <salomon.brys@gmail.com>
2026-04-07 13:14:29 +0200

    Dynamic density

--- composeApp/src/jvmMain/kotlin/fr/sb/pres_paug_0407/Presentation.kt
+++ composeApp/src/jvmMain/kotlin/fr/sb/pres_paug_0407/Presentation.kt
@@ -1,42 +1,76 @@
 package fr.sb.pres_paug_0407
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import kotlin.math.min
+
+val slideSize = IntSize(640, 360)
 
 @Composable
-@Preview
 fun Presentation() {
     MaterialTheme(
         colorScheme = darkColorScheme(),
     ) {
+        val outsideDensity by rememberUpdatedState(LocalDensity.current)
+        var insideDensity by remember { mutableStateOf(outsideDensity) }
         Surface(
             modifier = Modifier
                 .fillMaxSize()
+                .onSizeChanged {
+                    val wRatio = it.width / slideSize.width.toFloat()
+                    val hRatio = it.height / slideSize.height.toFloat()
+                    val ratio = min(wRatio, hRatio)
+                    insideDensity = Density(ratio)
+                }
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier
-                    .fillMaxSize()
+            CompositionLocalProvider(
+                LocalDensity provides insideDensity
             ) {
-                Text(
-                    text = "Compose beyond UI:",
-                    fontSize = 128.sp,
-                )
-                Text(
-                    text = "Display and Print.",
-                    fontSize = 96.sp,
-                )
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .size(slideSize.width.dp, slideSize.height.dp)
+                            .border(2.dp, Color.White)
+                    ) {
+                        Text(
+                            text = "Compose beyond UI:",
+                            style = MaterialTheme.typography.headlineMedium,
+                        )
+                        Text(
+                            text = "Display and Print.",
+                            style = MaterialTheme.typography.headlineSmall,
+                        )
+                    }
+                }
             }
         }
 
